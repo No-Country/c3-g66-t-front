c32146b9ce482abd53c2de4dcb0df50f399ca0d0
LisandroPer <87787218+LisandroPer@users.noreply.github.com>
2022-02-25 16:54:40 -0300

    Arreglo del linkeado responsive. Ya se puede acceder a Alojamiento

--- src/components/organisms/Navbar/MenuItems.js
+++ src/components/organisms/Navbar/MenuItems.js
@@ -8,7 +8,7 @@ const MenuItems = ({showMenu, show}) =>{
                <ul className={show ? "flex-col flex items-center fixed inset-0 left-1/4 gap-8 justify-center p-8  bg-black/20 backdrop-blur-lg  md:hidden" : "hidden"}>
                  <Close onClick={showMenu} className="courser-pointer"/>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/">Alojamiento</Link></li>
+                    <li><Link to="/hotels">Alojamiento</Link></li>
                     <li><Link to="/">Contacto</Link></li>
                     <li><Link to="/login">Ingresar</Link></li>
                     <li><Link to="/register">Registrarse</Link></li>
