3c0e30040e4285ffefb269808ecd40452c684704
LisandroPer <87787218+LisandroPer@users.noreply.github.com>
2022-03-02 23:45:33 -0300

    Se agregó la info traida de la Api

--- src/components/organisms/CardSection/HotelListContainer.js
+++ src/components/organisms/CardSection/HotelListContainer.js
@@ -7,7 +7,7 @@ const HotelListContainer = () => {
     const [hotels, setHotels] = useState([]);
 
     useEffect(()=> {
-        fetch("https://nc-hotely.herokuapp.com/api/v1/location/:location")
+        fetch("https://nc-hotely.herokuapp.com/api/v1/hotel/list?latitude=-22.911&longitude=-43.2094&checkin_date=2022-03-05&checkout_date=2022-03-19&sort_order=STAR_RATING_HIGHEST_FIRST&adults_number=7&page_number=1&currency=ARS&locale=es_ES")
          .then(respuesta => respuesta.json())
          .then(respuesta => setHotels(respuesta.data))
          .catch(err => alert(err))
