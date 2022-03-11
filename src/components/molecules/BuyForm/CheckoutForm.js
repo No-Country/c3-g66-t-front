7d79099f35ae426f6e4af3bc95ab83945ec2bf69
Brian Delgado <braiandelgado4255@hotmail.com>
2022-03-11 14:09:20 -0300

    Formulario de pago

--- src/components/molecules/BuyForm/CheckoutForm.js
+++ src/components/molecules/BuyForm/CheckoutForm.js
@@ -1,25 +1,53 @@
-import {PaymentElement, useElements, useStripe} from '@stripe/react-stripe-js';
+import {
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { useState } from "react";
+import { registerPaidReservation } from "../../../services/hotel";
 
-
-
-function CheckoutForm (){
+function CheckoutForm({ hotel_id, checkin_date, checkout_date, payment_id }) {
   const stripe = useStripe();
   const elements = useElements();
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const result = await stripe.confirmPayment({
+  const [paymentStatus, setPaymentStatus] = useState(false);
+
+  const handleSubmit = async event => {
+    try {
+      event.preventDefault();
+      const result = await stripe.confirmPayment({
         elements,
         confirmParams: {
-        return_url: "https://example.com/order/123/complete",
+          return_url: "https://example.com/order/123/complete",
         },
-    });  
-    console.log(result);
+        redirect: "if_required",
+      });
+      console.log(result);
+      const res = await registerPaidReservation({
+        hotel_id,
+        checkin_date,
+        checkout_date,
+        payment_id: result.paymentIntent.id,
+      });
+      elements.getElement(PaymentElement).destroy();
+      setPaymentStatus(true);
+      return;
+    } catch (err) {
+      console.log(err);
+      alert("Algo salio mal, intenta mas tarde");
+    }
   };
 
-  return(<form onSubmit={handleSubmit}>
-          <PaymentElement />
-          <button disabled={!stripe} className='button min-w-full mt-2'>Reservar</button>
-      </form>  
+  return (
+    <form onSubmit={handleSubmit}>
+      <PaymentElement />
+      {paymentStatus ? (
+        <div className="button min-w-full mt-2 bg-green-500">Pago Exitoso</div>
+      ) : (
+        <button disabled={!stripe} className="button min-w-full mt-2">
+          Reservar
+        </button>
+      )}
+    </form>
   );
 }
 
--- src/components/organisms/PaymentElement/index.js
+++ src/components/organisms/PaymentElement/index.js
@@ -1,22 +1,80 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import { useState } from "react";
+import { paymentFormDataService } from "../../../services/hotel";
+import Loader from "../../atoms/Loader";
 
 import CheckoutForm from "../../molecules/BuyForm/CheckoutForm";
 
 const stripePromise = loadStripe(
   "pk_test_51KalcSBGm1w1OkqgMrJr3o8GJtmwxSljglxv5sxMGibqbkdQxS1r9ege3WQmV2eEQBHNHvqKhwuaXuJSJWqympbh00tRk2bam9"
 );
+const PaymentElement = ({
+  hotel_id,
+  checkin_date,
+  checkout_date,
+  amount,
+  currency,
+  logged,
+}) => {
+  const [loadingStripe, setLoadingStripe] = useState(false);
+  const [dataForm, setDataForm] = useState({
+    ready: false,
+    clientSecret: undefined,
+    payment_id: undefined,
+  });
+  //carga la data necesaria del servidor para renderizar el form
+  const loadPaymentForm = async evt => {
+    try {
+      evt.preventDefault();
+      if (!logged) {
+        alert("Usuario no autenticado");
+        return;
+      }
+      setLoadingStripe(true);
+      const checkin = new Date(checkin_date).getTime();
+      const checkout = new Date(checkout_date).getTime();
+      const totalAmount =
+        amount * Math.ceil((checkout - checkin) / (1000 * 3600 * 24));
 
-const PaymentElement = () => {
-  const options = {
-    clientSecret:
-      "pi_3Kc0ODBGm1w1Okqg1N8vd8Aj_secret_VZDpNRCxKu9eXJd9AfCqXxXhH",
+      const res = await paymentFormDataService({
+        hotel_id,
+        checkin_date,
+        checkout_date,
+        amount: totalAmount,
+        currency: "ARS",
+      });
+      setDataForm({
+        ready: true,
+        clientSecret: res.client_secret,
+        payment_id: res.id,
+      });
+      setLoadingStripe(false);
+    } catch (err) {
+      alert("Algo salio mal, intenta mas tarde");
+      setLoadingStripe(false);
+    }
   };
 
-  return (
+  const options = {
+    clientSecret: dataForm.clientSecret,
+  };
+  return dataForm.ready ? (
     <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm />
+      <CheckoutForm
+        hotel_id={hotel_id}
+        checkin_date={checkin_date}
+        checkout_date={checkout_date}
+      />
     </Elements>
+  ) : (
+    <button
+      disabled={loadingStripe}
+      className="button min-w-full mt-2"
+      onClick={loadPaymentForm}
+    >
+      {loadingStripe ? "Cargando..." : "Reservar"}
+    </button>
   );
 };
 
